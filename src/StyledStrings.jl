df3cd91d42ed0665edfbfc5bb495b41699de3236
Lilith Hafner <Lilith.Hafner@gmail.com>
2023-10-26 08:34:06 -0500

    add useless comment

--- src/StyledStrings.jl
+++ src/StyledStrings.jl
@@ -1,5 +1,7 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
+# Hi!
+
 module StyledStrings
 
 import Base: AnnotatedString, AnnotatedChar, annotations, annotate!,
